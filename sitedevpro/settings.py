2c264719f90844671d74323a9a127e2416a6581c
Pedro <necromancerdoidao@outlook.com>
2023-02-04 10:08:56 -0300

    criados testes com o pytest-django close #6

--- sitedevpro/settings.py
+++ sitedevpro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sitedevpro.base'
 
 ]
 
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://DjangoPro.fly.dev']
