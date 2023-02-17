82e39eeffa1ec5ca21e8a441e543ef138c90a944
Pedro <necromancerdoidao@outlook.com>
2023-02-17 11:06:40 -0300

    instalados arquivos estáticos localmente
    close #35

--- sitedevpro/base/views.py
+++ sitedevpro/base/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
 
