c796e6015221c4a3dd5088618dcd040847d3fd04
Pedro <necromancerdoidao@outlook.com>
2023-02-25 10:02:16 -0300

    criados modelos automáticos com o model_mommy, finalizados testes de vídeo e criadas migrações
    close #72, close #73

--- sitedevpro/aperitivos/tests/test_video.py
+++ sitedevpro/aperitivos/tests/test_video.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
 
 from sitedevpro.aperitivos.models import Video
 from sitedevpro.django_assertions import assert_contains
@@ -7,9 +8,7 @@ from sitedevpro.django_assertions import assert_contains
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='2aYplgJrPDU')
-    v.save()
-    return v
+    return mommy.make(Video)
 
 
 @pytest.fixture
@@ -17,22 +16,22 @@ def resp(client, video):
     return client.get(reverse('aperitivos:video', args=(video.slug,)))
 
 
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug + 'video_nao_existente',)))
+
+
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-@pytest.fixture
-def resp_video_nao_encontrado(client, video):
-    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
-
-
-def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+def test_status_code_pagina_video_nao_encontrada(resp_video_nao_encontrado):
     assert resp_video_nao_encontrado.status_code == 404
 
 
-def test_titulo_video(resp, video):
+def test_title_video(resp, video):
     assert_contains(resp, video.titulo)
 
 
 def test_conteudo_video(resp, video):
-    assert_contains(resp, f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{video.youtube_id}"')
+    assert_contains(resp, f'src="https://www.youtube.com/embed/{video.youtube_id}"')
