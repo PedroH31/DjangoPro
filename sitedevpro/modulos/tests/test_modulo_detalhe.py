ee96eb84744a8086eb8f07159a3ad7390c41e0eb
Pedro <necromancerdoidao@outlook.com>
2023-02-27 11:38:01 -0300

    Criado relacionamento entre módulos e aulas, acessada relação com atributo dinâmico set
    close #86, close #87

--- sitedevpro/modulos/tests/test_modulo_detalhe.py
+++ sitedevpro/modulos/tests/test_modulo_detalhe.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from model_mommy import mommy
 
 from sitedevpro.django_assertions import assert_contains
-from sitedevpro.modulos.models import Modulo
+from sitedevpro.modulos.models import Modulo, Aula
 
 
 @pytest.fixture
@@ -12,7 +12,12 @@ def modulo(db):
 
 
 @pytest.fixture
-def resp(client, modulo):
+def aulas(modulo):
+    return mommy.make(Aula, 3, modulo=modulo)
+
+
+@pytest.fixture
+def resp(client, modulo, aulas):
     resp = client.get(reverse('modulos:detalhe', kwargs={'slug': modulo.slug}))
     return resp
 
@@ -29,3 +34,6 @@ def test_publico(resp, modulo: Modulo):
     assert_contains(resp, modulo.publico)
 
 
+def test_aulas_titulos(resp, aulas):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
