2196c4e94948b96cb8ddcc29dfcb5ed825aa6a54
Pedro <necromancerdoidao@outlook.com>
2023-02-28 12:36:51 -0300

    criada página de indices para os módulos, implementado método prefetch_related e objeto Prefetch para diminuir o número de queries a partir da faixada
    close #99

--- sitedevpro/modulos/views.py
+++ sitedevpro/modulos/views.py
@@ -9,6 +9,11 @@ def detalhe(request, slug):
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
 
 
+def indice(request):
+    ctx = {'modulos': facade.listar_modulos_com_aulas()}
+    return render(request, 'modulos/indice.html', ctx)
+
+
 def aula(request, slug):
     aula = facade.encontrar_aula(slug)
     return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
