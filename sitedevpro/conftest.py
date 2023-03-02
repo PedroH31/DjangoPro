ddd936e00c4ded160e095c4a8f58c47afb85ece0
Pedro <necromancerdoidao@outlook.com>
2023-03-02 08:05:18 -0300

    Inserido botão de login, criados testes com o usuário logado
    close #113, close #114

---
+++ sitedevpro/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from model_mommy import mommy
+
+
+@pytest.fixture
+def usuario_logado(db, django_user_model):
+    usuario_modelo = mommy.make(django_user_model)
+    return usuario_modelo
+
+
+@pytest.fixture
+def client_com_usuario_logado(usuario_logado, client):
+    client.force_login(usuario_logado)
+    return client
+
+
